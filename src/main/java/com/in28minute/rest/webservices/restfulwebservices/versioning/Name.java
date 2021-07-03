510ceb9f218b9a8b69b05d935248250bdc77cf30
mekhan <margub.khan@gmail.com>
2021-07-03 21:39:17 +0530

    40_ implementing versioning using different uri for different version of Person

---
+++ src/main/java/com/in28minute/rest/webservices/restfulwebservices/versioning/Name.java
@@ -0,0 +1,30 @@
+package com.in28minute.rest.webservices.restfulwebservices.versioning;
+
+public class Name {
+    private String firstName;
+    private String secondName;
+
+    public Name() {
+    }
+
+    public Name(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+}
