ec2b8f97c4dc4636e6c8fc195be81eb77e3fc23a
mekhan <margub.khan@gmail.com>
2021-07-03 21:55:24 +0530

    41_ other way fofimplementing versioning except different uri for different version of Person

--- src/main/java/com/in28minute/rest/webservices/restfulwebservices/versioning/PersonV1.java
+++ src/main/java/com/in28minute/rest/webservices/restfulwebservices/versioning/PersonV1.java
@@ -4,6 +4,9 @@ public class PersonV1 {
 
     private String name;
 
+    public PersonV1() {
+    }
+
     public PersonV1(String name) {
         this.name = name;
     }
--- src/main/java/com/in28minute/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/in28minute/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
+    //URI type version
     @GetMapping("v1/person")
     public PersonV1 personV1() {
         return new PersonV1("Bob Charlie");
@@ -16,4 +17,40 @@ public class PersonVersioningController {
         return new PersonV2(new Name("Bob", "Charlie"));
 
     }
+
+    // param versioning: /person/param?version=1; return following
+    @GetMapping(value = "/person/param", params = "version=1")
+    public PersonV1 paramV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/param", params = "version=2")
+    public PersonV2 paramV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+
+    }
+    //Header versioning
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 headerV1() {
+        return new PersonV1("Bob Charlie");
+    }
+    // MIME Type Version
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 headerV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+
+    }
+    //produces; what kind of json , application want to accept
+    @GetMapping(value = "/person/produces",produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(value = "/person/produces",produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+
+    }
+
+
 }
